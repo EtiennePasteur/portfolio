3eae7b4a494b5d57acf4d2a0987b4c566a3ccb78
Etienne Pasteur <me@etiennepasteur.com>
2020-01-08 00:28:02 +0100

    :construction: Add first work vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,26 +2,21 @@
   <div>
     <b-navbar toggleable="sm">
       <div class="container">
-        <b-navbar-brand>ETIENNE P.</b-navbar-brand>
+        <b-navbar-brand href="/">ETIENNE P.</b-navbar-brand>
         <b-navbar-toggle target="nav-collapse">
           <img src="~/assets/img/menu-icon.png" alt="menu" width="22" />
         </b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-auto">
-            <b-nav-item class="home text-center" href="#">HOME</b-nav-item>
+            <b-nav-item class="home text-center" href="/">HOME</b-nav-item>
             <b-nav-item-dropdown class="text-center" right text="WORK" toggle-class="nav-link-custom">
-              <b-dropdown-item href="#">April</b-dropdown-item>
+              <b-dropdown-item href="/april">April</b-dropdown-item>
               <b-dropdown-item href="#">Neo9</b-dropdown-item>
               <b-dropdown-item href="#">Ubg</b-dropdown-item>
               <b-dropdown-item href="#">Sogilis</b-dropdown-item>
             </b-nav-item-dropdown>
-            <b-nav-item-dropdown class="text-center" right text="PROJECTS" toggle-class="nav-link-custom">
-              <b-dropdown-item href="#">April</b-dropdown-item>
-              <b-dropdown-item href="#">Neo9</b-dropdown-item>
-              <b-dropdown-item href="#">Ubg</b-dropdown-item>
-              <b-dropdown-item href="#">Sogilis</b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item class="text-center" href="#">ABOUT</b-nav-item>
+            <b-nav-item class="text-center" href="/">PROJECTS</b-nav-item>
+            <b-nav-item class="text-center" href="/">ABOUT</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </div>
---
+++ pages/april.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="work">
+    <div class="work-header background-blue">
+      <div class="container d-flex justify-content-center align-items-center">
+        <img alt="neo9" class="img-neo9" src="~/assets/img/Neo9.png" />
+      </div>
+    </div>
+    <div class="work-body">
+      <div class="container">
+        <div class="work-body-title">AprilOn</div>
+        <div class="work-body-desc d-flex flex-row justify-content-between">
+          <div class="overview d-flex flex-column">
+            <div class="overview-title">
+              OVERVIEW
+            </div>
+            <div class="overview-desc">
+              <div>Intégrer à une équipe d'une dizaine de développeurs, j'ai participé au développement de l'extranet courtier d'APRIL (AprilOn) ainsi qu'au développement de sa Marketplace multi-marque.</div>
+              <div>Du lancement du projet jusqu'à sa mise en production, puis durant sa phase d'exploitation, j'ai eu la chance de passer par toutes les phases de vie du projet.</div>
+            </div>
+          </div>
+          <div class="role d-flex flex-column">
+            <div class="role-title">
+              MY ROLE
+            </div>
+            <div class="role-desc"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="spacer"></div>
+  </div>
+</template>
+
+<style lang="scss">
+  .work {
+    font-family: 'Avenir Next', sans-serif;
+
+    .background-blue {
+      background-color: #99cbfa;
+
+      div {
+        padding: 80px 0;
+
+        .img-neo9 {
+          width: 560px;
+        }
+      }
+    }
+
+    &-body {
+      margin: 60px 0;
+
+      &-title {
+        font-weight: 600;
+        font-size: 3vw;
+        line-height: 3.5vw;
+        letter-spacing: 0.4px;
+        padding-bottom: 50px;
+      }
+
+      &-desc {
+        .overview {
+          flex: 2;
+          margin-right: 30px;
+
+          &-title {
+            color: #757678;
+            font-size: 14px;
+            line-height: 24px;
+            font-weight: 400;
+            letter-spacing: 1px;
+            text-transform: uppercase;
+            margin-bottom: 20px;
+          }
+        }
+
+        .role {
+          flex: 1;
+          margin-left: 30px;
+
+          &-title {
+            color: #757678;
+            font-size: 14px;
+            line-height: 24px;
+            font-weight: 400;
+            letter-spacing: 1px;
+            text-transform: uppercase;
+            margin-bottom: 20px;
+          }
+        }
+      }
+    }
+
+    .spacer {
+      margin-right: 100px;
+      margin-left: 100px;
+      border-style: solid;
+      border-width: 0.5px;
+      border-color: #ccc;
+    }
+  }
+</style>
